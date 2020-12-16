ccb6a0ee4a45a63b900cadb12e4b6f13dcf70668
mattmax1 <matthieukoebsch@hotmail.com>
2020-12-16 15:20:36 +0100

    TextField/TextArea

--- projetJava/src/IHM/MaFenetre.java
+++ projetJava/src/IHM/MaFenetre.java
@@ -30,12 +30,32 @@ public class MaFenetre extends JFrame {
         JLabel label5 = new JLabel("Rangée");
         JLabel label6 = new JLabel("Résumé");
 
+        JTextField textField1 = new JTextField();
+        textField1.setPreferredSize(new Dimension(100,25));
+        JTextField textField2 = new JTextField();
+        textField2.setPreferredSize(new Dimension(100,25));
+        JTextField textField3 = new JTextField();
+        textField3.setPreferredSize(new Dimension(100,25));
+        JTextField textField4 = new JTextField();
+        textField4.setPreferredSize(new Dimension(100,25));
+        JTextField textField5 = new JTextField();
+        textField5.setPreferredSize(new Dimension(100,25));
+        JTextArea textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(100,150));
+
+
         panel.add(label1);
+        panel.add(textField1);
         panel.add(label2);
+        panel.add(textField2);
         panel.add(label3);
+        panel.add(textField3);
         panel.add(label4);
+        panel.add(textField4);
         panel.add(label5);
+        panel.add(textField5);
         panel.add(label6);
+        panel.add(textArea);
 
 
     }
